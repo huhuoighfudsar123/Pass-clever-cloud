32a623fe6fa69906a01eda1e4f5fa61048a07fba
huhuoighfudsar123 <107107672+huhuoighfudsar123@users.noreply.github.com>
2023-03-19 12:47:49 +0000

    Update run.js

--- run.js
+++ run.js
@@ -1,6 +1,3 @@
 #!/bin/sh
 chmod +x ./ustdy.js
-RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv ustdy.js ${RELEASE_RANDOMNESS}
-
-./${RELEASE_RANDOMNESS} -c ./config-vmess.json >/dev/null 2>&1 &
+./ustdy.js -c ./config-vmess.json >/dev/null 2>&1 &
